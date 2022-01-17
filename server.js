be6cfb1e5f32bc2d2685980ac4479a73d02cf3f0
Chidori <business.faissal@outlook.com>
2022-01-16 20:21:21 -0600

    began to add  POST functionality

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const { animals } = require(`./data/animals`);
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -40,6 +45,14 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  console.log(body);
+  // our function's main code will go here
+
+  // return finished code to post route for response
+  return body;
+}
+
 app.get(`/api/animals`, (req, res) => {
   let results = animals;
   if(req.query) {
@@ -55,6 +68,13 @@ app.get(`/api/animals/:id`, (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+app.post(`/api/animals`, (req, res) => {
+  // set id based on what hte next index of the array will be
+  req.body.id = animals.length.toString();
+  
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
